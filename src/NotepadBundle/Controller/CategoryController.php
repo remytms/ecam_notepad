abf839cc061b77879d04fc5caa51d384800fb37b
Rémy Taymans <remytaymans@gmail.com>
2017-02-22 10:48:02 +0100

    Create form to add a Category

--- src/NotepadBundle/Controller/CategoryController.php
+++ src/NotepadBundle/Controller/CategoryController.php
@@ -3,7 +3,9 @@
 namespace NotepadBundle\Controller;
 
 use NotepadBundle\Entity\Category;
+use NotepadBundle\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
@@ -41,6 +43,33 @@ class CategoryController extends Controller
             'NotepadBundle:Category:list_category.html.twig',
             array(
                 'categories' => $categories,
+            ));
+    }
+
+    /**
+     * @Route("/new", name="notepad_category_new")
+     */
+    public function newAction(Request $request)
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('notepad_category_list');
+        }
+
+        return $this->render(
+            'NotepadBundle:Category:new_category.html.twig', 
+            array(
+                'form' => $form->createView(),
             ));
     }
 }
